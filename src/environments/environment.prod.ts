c643023389e09c03840cc0cbc10a0fdc78e3bdc1
Erdi Köse <me@erdkse.com>
2024-05-24 00:28:50 +0300

    chore: add production value as hardcoded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const NG_APP_FIREBASE_CONFIG = import.meta.env.NG_APP_FIREBASE_CONFIG;
 const NG_APP_GA_ID = import.meta.env.NG_APP_GA_ID;
 
 export const environment = {
-    production: import.meta.env.PROD,
+    production: true,
     GA_ID: NG_APP_GA_ID,
     FIREBASE_CONFIG: JSON.parse(NG_APP_FIREBASE_CONFIG)
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ const NG_APP_FIREBASE_CONFIG = import.meta.env.NG_APP_FIREBASE_CONFIG;
 const NG_APP_GA_ID = import.meta.env.NG_APP_GA_ID;
 
 export const environment = {
-    production: import.meta.env.PROD,
+    production: false,
     GA_ID: NG_APP_GA_ID,
     FIREBASE_CONFIG: NG_APP_FIREBASE_CONFIG
 };
